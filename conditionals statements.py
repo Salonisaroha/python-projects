98798730b1f900e68005f9819be89017c511b60c
Saloni <115524019+Salonisaroha@users.noreply.github.com>
2023-02-18 17:08:21 +0530

    conditionals statements

---
+++ conditionals statements.py
@@ -0,0 +1,63 @@
+# If else and elif in python.
+"""age = input("enter your age")
+age = int(age)
+if (age>=18 and age<=40):
+    print("you are young") # The spaces before print keyword is that it follows indentation rule.
+elif (age>=41 and age<80) :
+    print("you are old")
+else :
+    print("you are very old.") # else is optional.
+
+# Relational operators :-
+# 1. ==
+# 2. >=
+# 3. <=
+
+# Logical operators :-
+
+# 1. and
+# 2. or
+# 3. not
+a = None
+if(a is None):
+    print("yes")
+else : 
+    print("no")
+
+x = [23,4,5,67,89]
+print(65 in x) # in keyword is used to find  that particular value in given code which you want to find.
+
+num1 = int(input("enter ist num"))
+num2 = int(input("enter 2nd num."))
+num3 = int(input("enter 3rd num."))
+num4 = int(input("enter 4th num."))
+if (num1>num4):
+    f1 = num1
+else:
+    f1 = num4
+if(num2>num3):
+    f2 = num2
+else:
+    f2 = num3
+if(f1>f2):
+    print(str((f1) + "is greatest"))
+else:
+    print((str(f2) + "is greatest"))
+
+sub1 = int(input("enter first subject marks\n"))
+sub2 = int(input("enter second subject marks\n"))
+sub3 = int(input("enter third subject marks\n"))
+if(sub1<33 or sub2<33 or sub3<33):
+    print("you are fail because you have less than 33 percent marks in one of the subjects")
+elif(sub1+sub2+sub3)/3<40:
+    print("you are fail")
+else:
+   print("you are pass.")"""
+
+name = "my name is saloni"
+sirname = " saroha"
+print(name + sirname) 
+
+
+
+
